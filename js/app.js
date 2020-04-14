dd61b6dea8837073104e51b6c2ba18d3c5571f1a
Ashley <ashleybiermann@gmail.com>
2020-04-14 14:32:05 -0700

    connected chart and have the boiler plate chart showing

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 // =====================================
 
 // RenderToPage method attached to constructor
-
+//TODO: attach these to the object, so they are no longer global
 var allProductsArr = new Array;
 var totalVotes = 0;
 var maxVotes = 6;
@@ -105,3 +105,22 @@ function handleClickOnProduct(event) {
   }
 }
 
+var ctx = document.getElementById('productDataChart').getContext('2d');
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
